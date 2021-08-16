78ba4c58207e4fc0f8bf2ad9d4cabcee0f734721
Jetty2020 <kmclo@naver.com>
2021-08-16 22:34:23 +0900

    [feat] Login mutation

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+import { LOCALSTORAGE_TOKEN } from './constants';
 
+const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(false);
+export const authTokenVar = makeVar(token);
 
 export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { gql } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 import { useForm } from 'react-hook-form';
 import PageTitle from '../components/PageTitle';
 import { LoginInput } from '../__generated__/globalTypes';
+import {
+  loginMutation,
+  loginMutationVariables,
+} from '../__generated__/loginMutation';
+import { LOCALSTORAGE_TOKEN } from '../constants';
+import { authTokenVar, isLoggedInVar } from '../apollo';
 
-export const LOGIN_MUTATION = gql`
+const LOGIN_MUTATION = gql`
   mutation loginMutation($loginInput: LoginInput!) {
     login(input: $loginInput) {
       ok
@@ -23,9 +29,34 @@ const Login = () => {
   } = useForm<LoginInput>({
     mode: 'onChange',
   });
+  const onCompleted = (data: loginMutation) => {
+    const {
+      login: { ok, token },
+    } = data;
+    if (ok && token) {
+      localStorage.setItem(LOCALSTORAGE_TOKEN, token);
+      authTokenVar(token);
+      isLoggedInVar(true);
+    }
+  };
+  const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTATION, {
+    onCompleted,
+  });
   const onSubmit = () => {
-    const { email, password } = getValues();
-    console.log(email, password);
+    if (!loading) {
+      const { email, password } = getValues();
+      loginMutation({
+        variables: {
+          loginInput: {
+            email,
+            password,
+          },
+        },
+      });
+    }
   };
   return (
     <div>
@@ -34,7 +65,7 @@ const Login = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <input
             {...register('email', {
-              required: '메일 작서칸이 비어있습니다..',
+              required: '메일 작서칸이 비어있습니다.',
               pattern:
                 /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             })}
@@ -43,6 +74,10 @@ const Login = () => {
             placeholder="Email"
             className="input"
           />
+          {errors.email?.type === 'pattern' && (
+            <div>메일 주소가 올바르지 않습니다.</div>
+          )}
+          {errors.email?.message && <div>{errors.email?.message}</div>}
           <input
             {...register('password', {
               required: '비밀번호 작성칸이 비어있습니다.',
@@ -52,6 +87,10 @@ const Login = () => {
             placeholder="Password"
             className="input"
           />
+          {errors.password?.message && <div>{errors.password?.message}</div>}
+          {loginMutationResult?.login.error && (
+            <div>{loginMutationResult.login.error}</div>
+          )}
           <button>Log in </button>
         </form>
       </div>
