592f22aa94ed44f416ca1791a95ca3cf2c0b5913
Jetty2020 <kmclo@naver.com>
2021-08-16 21:55:22 +0900

    [feat] Create login form UI(1)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,48 @@
-import { useReactiveVar } from '@apollo/client';
 import React from 'react';
-import { isLoggedInVar } from '../apollo';
+import { useForm } from 'react-hook-form';
 import PageTitle from '../components/PageTitle';
 
-type Props = {};
-const Login: React.FC<Props> = () => {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
+const Login = () => {
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: 'onChange',
+  });
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    console.log(email, password);
+  };
   return (
     <div>
       <PageTitle title="Login" />
-      {isLoggedIn ? <div>Login</div> : <div>Logout</div>}
-      <button onClick={() => isLoggedInVar(!isLoggedIn)}>login</button>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register('email', {
+              required: '메일 작서칸이 비어있습니다..',
+              pattern:
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="Email"
+            className="input"
+          />
+          <input
+            {...register('password', {
+              required: '비밀번호 작성칸이 비어있습니다.',
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="input"
+          />
+          <button>Log in </button>
+        </form>
+      </div>
     </div>
   );
 };
