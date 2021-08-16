e13cf9ee93e6757ca7a8901cc40106625f7e7fc8
Jetty2020 <kmclo@naver.com>
2021-08-16 21:27:51 +0900

    [feat] Global style setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,28 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import routes from './routes';
+import { ThemeProvider } from 'styled-components';
+import { theme, GlobalStyles } from './styles';
+
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <Switch>
-          <Route path={routes.home} exact>
-            <Home />
-          </Route>
-          <Route path={routes.login} exact>
-            <Login />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Router>
+          <Switch>
+            <Route path={routes.home} exact>
+              <Home />
+            </Route>
+            <Route path={routes.login} exact>
+              <Login />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
